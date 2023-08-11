772b71d23f736cf8a9a92b45989143e2c8495d7e
Tayyab Younas <tayyabyounas550@gmail.com>
2023-08-11 15:00:53 +0500

    images path seating by using markdown

--- Components/posts/post-detail/post-contact.js
+++ Components/posts/post-detail/post-contact.js
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown";
 
 import PostsHeader from "./post-header";
+import Image from "next/image";
 
 import classes from "./post-contact.module.css";
 
@@ -10,10 +11,43 @@ function PostsContact(props) {
   // const imagePath = `./images/posts/${post.slug}/${post.image}`;
   const imagePath = `/images/posts/${post.image}`;
 
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostsHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.contact}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.contact}</ReactMarkdown>
     </article>
   );
 }
